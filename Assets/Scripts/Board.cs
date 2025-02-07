ed8c28aaf6095a86b70c99fd557cd92913c7c4c4
Sol Elan <solipod8@gmail.com>
2025-02-07 20:00:29 +0200

    Add: Check drag direction

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,13 +4,14 @@ using UnityEngine.PlayerLoop;
 public class Board : MonoBehaviour
 {
     [SerializeField] private GameObject tilePrefab;
-    [SerializeField] private float dragDistance = 5f;
+    [SerializeField] private float dragDistance = .5f;
 
     private TileSO[,] boardGrid;
     private TilesCatalog tileCatalog;
 
     private bool _isDragging;
-    private Vector3 _startPosition;
+    private Vector3 _startDragPosition;
+    private Vector2 _selectedTilePosition;
 
     public void Initialize(TilesCatalog catalog, TileSO[,] grid)
     {
@@ -26,14 +27,19 @@ public class Board : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !_isDragging)
         {
             _isDragging = true;
-            _startPosition = currentPosition;
+            if (IsWithinGrid(currentPosition))
+            {
+                _startDragPosition = currentPosition;
+            }
         }
         
-        if (_isDragging && IsValidDrag(_startPosition, currentPosition))
+        if (Input.GetMouseButtonUp(0)) _isDragging = false;
+
+        if (_isDragging && IsValidDrag(_startDragPosition, currentPosition))
         {
-            Vector3 direction = GetDirection(_startPosition, currentPosition);
-            TileSO firstTile = boardGrid[Mathf.FloorToInt(_startPosition.x), Mathf.FloorToInt(_startPosition.y)];
-            TileSO secondTile = boardGrid[Mathf.FloorToInt(_startPosition.x) + (int)direction.x, Mathf.FloorToInt(_startPosition.y) + (int)direction.y];
+            Vector3 direction = GetDirection(_startDragPosition, currentPosition);
+            TileSO firstTile = boardGrid[Mathf.FloorToInt(_startDragPosition.x), Mathf.FloorToInt(-_startDragPosition.y)];
+            TileSO secondTile = boardGrid[Mathf.FloorToInt(_startDragPosition.x) + (int)direction.x, Mathf.FloorToInt(-_startDragPosition.y) - (int)direction.y];
             Debug.Log($"{firstTile.id} -> {secondTile.id}");
             _isDragging = false;
         }
@@ -54,16 +60,18 @@ public class Board : MonoBehaviour
     private bool IsWithinGrid(Vector3 position)
     {
         int x = Mathf.FloorToInt(position.x);
-        int y = Mathf.FloorToInt(position.y);
+        int y = Mathf.FloorToInt(position.y * -1); // To correct to grid values
         return x >= 0 && x < boardGrid.GetLength(0) && y >= 0 && y < boardGrid.GetLength(1);
     }
 
     private bool IsValidDrag(Vector3 startPosition, Vector3 endPosition)
     {
-        if (startPosition == endPosition) return false;
-        if (!IsWithinGrid(startPosition) || !IsWithinGrid(endPosition)) return false;
-
-        if (Vector3.Distance(_startPosition, Input.mousePosition) < dragDistance) return false;
+        if (
+            startPosition == endPosition
+            || Vector3.Distance(startPosition, Input.mousePosition) < dragDistance
+            || !IsWithinGrid(startPosition)
+            || !IsWithinGrid(endPosition)
+        ) return false;
 
         return true;
     }
@@ -82,11 +90,11 @@ public class Board : MonoBehaviour
             }
         }
     }
-    
+
     private Vector3 GetMouseWorldPosition()
     {
         Vector3 screenPosition = Input.mousePosition;
-        screenPosition.z = Camera.main.nearClipPlane;
+        screenPosition.z = 10f;
         return Camera.main.ScreenToWorldPoint(screenPosition);
     }
 
@@ -94,6 +102,6 @@ public class Board : MonoBehaviour
     {
         // Make the spawn at top left instead of center
         // this way the area of the tile is (0-1, 0-1, 0) instead of (-0.5-0.5, -0.5-0.5, 0)
-        return new Vector3(x, -y, 0f) + new Vector3(0.5f, -0.5f, 0f); 
+        return new Vector3(x, -y, 0f) + new Vector3(0.5f, -0.5f, 0f);
     }
 }
